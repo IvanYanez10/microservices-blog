4aa87cb1f17f1ead9786675ddaee7babcf405681
ivancr <iyanez717@gmail.com>
2021-10-10 23:28:49 -0500

    final with some painful things

--- posts/index.js
+++ posts/index.js
@@ -13,15 +13,15 @@ const posts = {};
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
-
-app.post('/posts', async (req, res) => {
+ 
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
     posts[id] = {
         id, title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://eventbus-srv:4005/events', {
         type: 'PostCreated', 
         data: {
             id,
